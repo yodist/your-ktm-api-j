80b9a1d2b85a7126f6f62e18c3ad66eaa2656ad7
Yodi Satriani <yodisatriani@live.com>
2019-10-08 03:25:39 +0800

    allow cross from localhost:3000

--- src/main/java/com/yodist/yourktm/controller/TrainController.java
+++ src/main/java/com/yodist/yourktm/controller/TrainController.java
@@ -8,6 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.yodist.yourktm.handler.ResponseHandler;
 import com.yodist.yourktm.repository.TrainRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/train")
 public class TrainController {
 
